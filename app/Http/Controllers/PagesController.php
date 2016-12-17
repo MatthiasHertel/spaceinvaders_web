82cfb9fe4629c6de4f96f0aea22b269e52302a63
Matthias Hertel <me@matthiashertel.eu>
2016-12-17 14:24:07 +0100

    refactor the old controllers + setup auth controller + make new routes (download , doku , startseite)

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Highscore;
+
+class PagesController extends Controller
+{
+    //
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // Startseite
+    public function getIndex()
+    {
+        return view('pages.startseite');
+    }
+    // Dokumentation
+    public function getDokumentation()
+    {
+        return view('pages.dokumentation');
+    }
+    // Download
+    public function getDownload()
+    {
+        return view('pages.download');
+    }
+    // Highscore page with Data from DB
+    public function getHighscore()
+    {
+      $highscores = Highscore::orderBy('id', 'desc')->paginate(10);
+      return view('pages.highscore')->withHighscores($highscores);
+    }
+}
