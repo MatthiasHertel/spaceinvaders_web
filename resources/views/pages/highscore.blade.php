68cad1f1ae25f24bd3b73ae1acf43bddd5c4d1bb
Matthias Hertel <me@matthiashertel.eu>
2016-12-17 23:21:50 +0100

    unify containers ... fix sth

--- resources/views/pages/highscore.blade.php
+++ resources/views/pages/highscore.blade.php
@@ -2,42 +2,40 @@
 
 @section('content')
 <div class="container">
-  <div class="container">
-    <div class="row">
-        <div class="col-md-12 ">
-            <div class="panel panel-default">
-                <div class="panel-heading">Highscore</div>
-                <div class="panel-body">
-                  <table class="table">
-                    <thead>
-                      <th>id</th>
-                      <th>name</th>
-                      <th>punkte</th>
-                      <th>created_at</th>
-                      <th>updated_at</th>
+  <div class="row">
+      <div class="col-md-12 ">
+          <div class="panel panel-default">
+              <div class="panel-heading">Highscore</div>
+              <div class="panel-body">
+                <table class="table">
+                  <thead>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>punkte</th>
+                    <th>created_at</th>
+                    <th>updated_at</th>
 
-                    </thead>
-                    <tbody>
-                      @foreach($highscores as $highscore)
-                      <tr>
-                        <td>{{ $highscore->id }}</td>
-                        <td>{{ $highscore->name }}</td>
-                        <td>{{ $highscore->punkte }}</td>
-                        <td>{{ $highscore->created_at }}</td>
-                        <td>{{ $highscore->updated_at }}</td>
-                      </tr>
-                      @endforeach
+                  </thead>
+                  <tbody>
+                    @foreach($highscores as $highscore)
+                    <tr>
+                      <td>{{ $highscore->id }}</td>
+                      <td>{{ $highscore->name }}</td>
+                      <td>{{ $highscore->punkte }}</td>
+                      <td>{{ $highscore->created_at }}</td>
+                      <td>{{ $highscore->updated_at }}</td>
+                    </tr>
+                    @endforeach
 
-                    </tbody>
-                  </table>
-                  <div class="text-center">
-                    {!! $highscores->links() !!}
-                  </div>
-
+                  </tbody>
+                </table>
+                <div class="text-center">
+                  {!! $highscores->links() !!}
                 </div>
-            </div>
-        </div>
-    </div>
+
+              </div>
+          </div>
+      </div>
   </div>
-</div>  
+</div>
 @endsection
