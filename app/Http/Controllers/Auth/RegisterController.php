7a6725d30e9cbd92df6af5a9101c053805f5c2e5
Matthias Hertel <me@matthiashertel.eu>
2016-12-18 19:26:21 +0100

    overwrite the REGISTER method from the registersusers trait in the registercontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,6 +73,37 @@ class RegisterController extends Controller
         ]);
     }
 
+    /**
+    *  Over-ridden the register method from the "RegistersUsers" trait
+    *  Remember to take care while upgrading laravel
+    */
+    public function register(Request $request)
+    {
+        // Laravel validation
+        $validator = $this->validator($request->all());
+        if ($validator->fails())
+        {
+            $this->throwValidationException($request, $validator);
+        }
+        // Using database transactions is useful here because stuff happening is actually a transaction
+        // I don't know what I said in the last line! Weird!
+        DB::beginTransaction();
+        try
+        {
+            $user = $this->create($request->all());
+            // After creating the user send an email with the random token generated in the create method above
+            $email = new EmailVerification(new User(['email_token' => $user->email_token]));
+            Mail::to($user->email)->send($email);
+            DB::commit();
+            return back();
+        }
+        catch(Exception $e)
+        {
+            DB::rollback();
+            return back();
+        }
+    }
+
     public function verify($token)
     {
         // The verified method has been added to the user model and chained here
