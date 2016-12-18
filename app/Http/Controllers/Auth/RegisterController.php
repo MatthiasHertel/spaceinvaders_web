71433b4779cf604b341ab394c1b2ca76bf4479ee
Matthias Hertel <me@matthiashertel.eu>
2016-12-18 19:22:38 +0100

    modify the create method for verification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'email_token' => str_random(10),
         ]);
     }
 
