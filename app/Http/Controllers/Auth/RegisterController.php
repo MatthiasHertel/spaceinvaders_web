5a231314df395bf65201fc79c19275c734a4f3e2
Matthias Hertel <me@matthiashertel.eu>
2016-12-18 19:03:50 +0100

    setup the verification token route and handle this in the registercontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,4 +68,13 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+
+    public function verify($token)
+    {
+        // The verified method has been added to the user model and chained here
+        // for better readability
+        User::where('email_token',$token)->firstOrFail()->verified();
+        return redirect('login');
+    }
+
 }
