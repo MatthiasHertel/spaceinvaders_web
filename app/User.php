568a1e77729c8e67781d659d9c5462e2f20e042e
Matthias Hertel <me@matthiashertel.eu>
2016-12-27 08:44:04 +0100

    fix sth in the User Model

--- app/User.php
+++ app/User.php
@@ -59,7 +59,7 @@ class User extends Authenticatable
 
     public function hasRole($role) {
       if ($this->roles()->where('name', $role)->first()) {
-        return false;
+        return true;
       }
       return false;
     }
