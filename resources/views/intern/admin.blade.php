3cff50ee53fffe59ad9d212703c3a412e0044601
Matthias Hertel <me@matthiashertel.eu>
2016-12-27 08:45:36 +0100

    setup adminview add form to manage user roles

--- resources/views/intern/admin.blade.php
+++ resources/views/intern/admin.blade.php
@@ -15,17 +15,30 @@
                     <th>verified</th>
                     <th>created_at</th>
                     <th>updated_at</th>
-
+                    <th>User</th>
+                    <th>Author</th>
+                    <th>Admin</th>
                   </thead>
                   <tbody>
                     @foreach($users as $user)
                     <tr>
-                      <td>{{ $user->id }}</td>
-                      <td>{{ $user->name }}</td>
-                      <td>{{ $user->email }}</td>
-                      <td>{{ $user->verified }}</td>
-                      <td>{{ $user->created_at }}</td>
-                      <td>{{ $user->updated_at }}</td>
+                      <form class="" action="index.html" method="post">
+                        <div class="form-group">
+                          <td>{{ $user->id }}</td>
+                          <td>{{ $user->name }}</td>
+                          <td>{{ $user->email }}<input type="hidden" name="email" value="{{ $user->email }}"></td>
+                          <td>{{ $user->verified }}</td>
+                          <td>{{ $user->created_at }}</td>
+                          <td>{{ $user->updated_at }}</td>
+                          <td><input type="checkbox" name="role_user" {{ $user->hasRole('user') ? 'checked' : '' }}></td>
+                          <td><input type="checkbox" name="role_author" {{ $user->hasRole('author') ? 'checked' : '' }}></td>
+                          <td><input type="checkbox" name="role_admin" {{ $user->hasRole('admin') ? 'checked' : '' }}></td>
+
+                          <td><button type="submit" class="btn-xs btn-default">Assign Roles</button> </td>
+                          {{ csrf_field() }}
+                        </div>
+
+                      </form>
                     </tr>
                     @endforeach
 
